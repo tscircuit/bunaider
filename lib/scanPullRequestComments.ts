ccc534d959d7229641b561df6869466746b53d71
seveibar <seveibar@gmail.com>
2024-07-26 12:24:59 -0700

    fix scanning pull requests

--- lib/scanPullRequestComments.ts
+++ lib/scanPullRequestComments.ts
@@ -1,21 +1,51 @@
 import type { PullRequestReviewComment } from "@octokit/webhooks-types"
 import { execSync } from "child_process"
+import type { Endpoints } from "@octokit/types"
 
 export async function scanPullRequestComments(prNumber, repoInfo) {
   let comments: Array<{
     body: string
-    submittedAt: string
-    state: "COMMENTED" | "DISMISSED" | "CHANGES_REQUESTED"
-    author: { login: string }
+    submittedAt?: string
+    state?: "COMMENTED" | "DISMISSED" | "CHANGES_REQUESTED"
+    author: { login?: string }
+    path?: string
     id: string
   }> = []
   if (repoInfo.useOctokit) {
-    const { data } = await repoInfo.octokit.pulls.listReviewComments({
+    const { data: commentData } =
+      (await repoInfo.octokit.pulls.listReviewComments({
+        owner: repoInfo.owner,
+        repo: repoInfo.repo,
+        pull_number: parseInt(prNumber),
+      })) as Endpoints["GET /repos/{owner}/{repo}/pulls/{pull_number}/comments"]["response"]
+    comments.push(
+      ...commentData.map((c) => ({
+        author: { login: c.user.login },
+        body: c.body,
+        id: c.id.toString(),
+        path: c.path,
+        submittedAt: c.created_at,
+      })),
+    )
+    // Pull in the reviews
+    const { data: reviewData } = (await repoInfo.octokit.pulls.listReviews({
       owner: repoInfo.owner,
       repo: repoInfo.repo,
       pull_number: parseInt(prNumber),
-    })
-    comments = data
+    })) as Endpoints["GET /repos/{owner}/{repo}/pulls/{pull_number}/reviews"]["response"]
+    comments.push(
+      ...reviewData.map((c) => ({
+        author: { login: c.user?.login },
+        body: c.body,
+        id: c.id.toString(),
+        submittedAt: c.submitted_at,
+        state: c.state as
+          | "COMMENTED"
+          | "DISMISSED"
+          | "CHANGES_REQUESTED"
+          | undefined,
+      })),
+    )
   } else {
     const prResRaw = execSync(
       `gh pr view ${prNumber} --json comments,reviews`,
